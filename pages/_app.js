29673d42ed3feff4451c1465ead0258d0f3f0491
古賀 靖典 <kogaktt@gmail.com>
2020-07-16 17:16:14 +0900

    サイト全体のメニューバー作成

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,86 @@ import React from 'react';
 import UserProvider from '../context/userContext';
 import theme from '../components/theme.ts';
 import { ThemeProvider } from '@material-ui/core';
+import Head from 'next/head';
+// app bar
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <UserProvider>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Head>
+          <title>Bark Walk</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <AppBar position="static" style={{ width: '100%', flexGrow: 1 }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleClick}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>
+                  ドッグウォーカーを探す
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  ドッグウォーカーになる
+                </MenuItem>
+                <MenuItem onClick={handleClose}>サービスについて</MenuItem>
+                <MenuItem onClick={handleClose}>会員登録</MenuItem>
+              </Menu>
+              <Typography variant="h6" style={{ flexGrow: 1 }}>
+                Bark Walk
+              </Typography>
+              <Button color="inherit">Sign Up</Button>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+          <Component {...pageProps} />
+        </body>
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
       </ThemeProvider>
     </UserProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Head from 'next/head';
 import { useEffect } from 'react';
 import { useUser } from '../context/userContext';
-import firebase from '../firebase/clientApp';
-import { Button } from '@material-ui/core';
+// import firebase from '../firebase/clientApp';
 
 export default function Home() {
   // Our custom hook to get context values
@@ -16,175 +14,18 @@ export default function Home() {
     }
   }, [loadingUser, user]);
 
-  const pushData = () => {
-    const db = firebase.firestore();
-    db.collection('walkers').add({
-      name: '山田 花子',
-      breedingYear: 12,
-    });
-  };
+  // Firebase接続確認用のテストコード
+  // const pushData = () => {
+  //   const db = firebase.firestore();
+  //   db.collection('walkers').add({
+  //     name: '山田 花子',
+  //     breedingYear: 12,
+  //   });
+  // };
 
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js w/ Firebase Client-Side</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">犬サイト</h1>
-        <Button
-          style={{ marginTop: '20px', color: '#FFF' }}
-          onClick={() => {
-            pushData();
-          }}
-          color="primary"
-          variant="contained"
-        >
-          テスト送信
-        </Button>
-      </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    <div>
+      <h1>TITLE</h1>
     </div>
   );
 }
