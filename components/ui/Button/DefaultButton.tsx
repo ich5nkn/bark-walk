be8742e77cb782b395060651dc55390b9f202487
dyekv <kogaktt@gmail.com>
2020-10-16 20:14:43 +0900

    save

--- components/ui/Button/DefaultButton.tsx
+++ components/ui/Button/DefaultButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-const DefaultButton = ({ children,onClick=()=>{} }) => {
+const DefaultButton = ({ children,onClick=() => {} }) => {
   return (
     <Button variant="contained" color="primary" style={{ color: '#FFF' }} onClick={onClick}>
       {children}
