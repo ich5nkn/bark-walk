ffa7f45151d8e0d37cd1e763f6ba8b74ad924882
motikoma <motikoma5125@gmail.com>
2021-02-20 22:12:56 +0900

    fix:  Layoutで一括でloadingコンポーネントを表示するように修正

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Container from '@material-ui/core/Container';
+import { useUser } from '../context/userContext';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { Grid } from '@material-ui/core';
 // import { makeStyles } from '@material-ui/core';
 
 const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => {
   // const useStyles = makeStyles(() => {});
+  const { loadingUser } = useUser();
   return (
-    <>
-      <Container maxWidth="md">
-        <Navbar />
+    <Container maxWidth="md">
+      <Navbar />
+      {loadingUser ? (
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          style={{ height: '100vh' }}
+        >
+          <Grid item>
+            <CircularProgress />
+          </Grid>
+        </Grid>
+      ) : (
         <div style={{ marginTop: 80 }}>{children}</div>
-      </Container>
-    </>
+      )}
+    </Container>
   );
 };
 
