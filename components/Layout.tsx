ea3c4cf8180cb3219d05858a2fe3f59e5ecb286e
motikoma <motikoma5125@gmail.com>
2021-02-20 22:47:55 +0900

    fix: Layoutでloadingを表示する処理を追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Container from '@material-ui/core/Container';
-import { useUser } from '../context/userContext';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Grid } from '@material-ui/core';
 // import { makeStyles } from '@material-ui/core';
 
-const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => {
+interface Props {
+  loading?: boolean;
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<Props> = ({ children, loading }) => {
   // const useStyles = makeStyles(() => {});
-  const { loadingUser } = useUser();
   return (
     <Container maxWidth="md">
       <Navbar />
-      {loadingUser ? (
+      {loading ? (
         <Grid
           container
           justify="center"
