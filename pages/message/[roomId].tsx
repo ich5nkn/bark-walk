3da66a4f5298c5b0dcf924c6de4c8d680b9a6953
krevy <kogaktt@gmail.com>
2021-03-14 19:14:44 +0900

    add:チャットのバルーンを作成

--- pages/message/[roomId].tsx
+++ pages/message/[roomId].tsx
@@ -1,17 +1,93 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import * as firebase from 'firebase';
+import Layout from '../../components/Layout';
+import { useUser } from '../../context/userContext';
+import { makeStyles } from '@material-ui/core';
 
 interface messageObj {
   message: string;
   sender: string;
 }
 
+const useStyles = makeStyles(() => ({
+  chatLeft: {
+    position: 'relative',
+    display: 'inline-block',
+    margin: '1.5em 0 1.5em 15px',
+    padding: '7px 10px',
+    minWidth: '120px',
+    maxWidth: '100%',
+    color: '#555',
+    fontSize: '16px',
+    background: '#FFF',
+    border: 'solid 3px #555',
+    boxSizing: 'border-box',
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      left: '-24px',
+      marginTop: '-12px',
+      border: '12px solid transparent',
+      borderRight: '12px solid #FFF',
+      zIndex: '2',
+    },
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      left: '-30px',
+      marginTop: '-14px',
+      border: '14px solid transparent',
+      borderRight: '14px solid #555',
+      zIndex: '1',
+    },
+  },
+  chatRight: {
+    position: 'relative',
+    display: 'inline-block',
+    margin: '1.5em 15px 1.5em 0',
+    left: '100%',
+    transform: 'translateX(-100%)',
+    padding: '7px 10px',
+    minWidth: '120px',
+    maxWidth: '100%',
+    color: '#555',
+    fontSize: '16px',
+    background: '#FFF',
+    border: 'solid 3px #555',
+    boxSizing: 'border-box',
+    '&::before': {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      right: '-24px',
+      marginTop: '-12px',
+      border: '12px solid transparent',
+      borderLeft: '12px solid #FFF',
+      zIndex: '2',
+    },
+    '&::after': {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      right: '-30px',
+      marginTop: '-14px',
+      border: '14px solid transparent',
+      borderLeft: '14px solid #555',
+      zIndex: '1',
+    },
+  },
+}));
+
 const Message = (): JSX.Element => {
   const db = firebase.firestore();
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(true);
   const [messages, setMessages] = useState<messageObj[]>([]);
+  const { user } = useUser();
+  const classes = useStyles();
 
   useEffect(() => {
     if (router.asPath !== router.route) {
@@ -35,15 +111,20 @@ const Message = (): JSX.Element => {
     }
   }, [router]);
 
-  if (loading) {
-    return <div>loading...</div>;
-  }
   return (
-    <div>
+    <Layout loading={loading}>
       {messages.map((message, idx) => (
-        <div key={idx}>{message.message}</div>
+        <div key={idx}>
+          <div
+            className={
+              user.uid == message.sender ? classes.chatRight : classes.chatLeft
+            }
+          >
+            {message.message}
+          </div>
+        </div>
       ))}
-    </div>
+    </Layout>
   );
 };
 
